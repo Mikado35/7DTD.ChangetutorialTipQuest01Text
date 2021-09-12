90892346cb45cfa41478e766fcd31ec5e2431486
Mikado35 <36087166+Mikado35@users.noreply.github.com>
2021-09-12 10:06:16 +0200

    Version 1.0

---
+++ zzzzzzzzzz 0-DMT Mikado Change tutorialTipQuest01 Text/Harmony/ChangetutorialTipQuest01Text.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using DMT;
+using UnityEngine;
+
+namespace Harmony_Mikado_Core
+{
+
+	public class ChangetutorialTipQuest01Text
+	{
+        public class Init : IHarmony
+        {
+            public void Start()
+            {
+                Debug.Log((object)("Mikado Loading Patch : " + this.GetType().ToString()));
+                new Harmony(this.GetType().ToString()).PatchAll(Assembly.GetExecutingAssembly());
+            }
+        }
+
+		// Removes the Tool Tips for Journal
+		[HarmonyPatch(typeof(XUiC_TipWindow))]
+		[HarmonyPatch("ShowTip")]
+		public class XUiC_TipWindow_ShowTip
+		{
+			static bool Prefix(ref string tip, EntityPlayerLocal _localPlayer, ToolTipEvent closeEvent)
+			{
+				if (tip == "tutorialTipQuest01")
+					tip = "alt_tutorialTipQuest01";
+				return true;
+			}
+		}
+	}
+}
